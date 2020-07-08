4061d6f3a93139777fc4b856cdcad4bf1621a665
erianvc <erianvc.dev@gmail.com>
2020-07-08 12:25:19 -0500

    release: v1.0.0

---
+++ README.md
@@ -0,0 +1,5 @@
+# Social Media Dashboard
+
+Challenge #3 by [Frontend Mentor](https://www.frontendmentor.io/challenges)
+
+![Design preview for Social Media Dashboard](https://repository-images.githubusercontent.com/278151444/b1390700-c115-11ea-9937-4c7e7ec6afdf)
---
+++ src/main.js
@@ -0,0 +1,15 @@
+'use strict'
+const checkbox = document.getElementById('checkbox')
+
+if (window.matchMedia('(prefers-color-scheme: dark)').matches)
+    checkbox.setAttribute('checked', true)
+
+checkbox.addEventListener('change', function (event) {
+    if (this.checked) {
+        document.body.classList.remove('light-theme')
+        document.body.classList.add('dark-theme')
+    } else {
+        document.body.classList.remove('dark-theme')
+        document.body.classList.add('light-theme')
+    }
+})
